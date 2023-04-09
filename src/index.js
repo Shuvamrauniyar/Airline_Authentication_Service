22b9fa47ea8ee4fc15ef408d1c7337a992fe3cb7
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2023-04-10 00:36:03 +0530

    added roles model and seeded it and associated roles table with users table using user_roles through table

--- src/index.js
+++ src/index.js
@@ -6,13 +6,28 @@ const {PORT} = require('./config/ServerConfig');
 const ApiRoutes = require('./routes/index');
 const bodyParser = require('body-parser');
 
-const startServer = () => {
+const db = require('./models/index');
+
+const {User,Role} = require('./models/index');
+
+ const startServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
     app.use('/api',ApiRoutes);
-    app.listen(PORT,() => {
+    app.listen(PORT,async () => {
         console.log(`Server Started on ${PORT}`)
+        // if(process.env.DB_SYNC)
+        // {
+        //     db.sequelize.sync({alter:true});
+        // }
+
+        const u1 = await User.findByPk(5);
+        const r1 = await Role.findByPk(1);
+        // u1.addRoles(r1);//this and below works same
+        r1.addUsers(u1);
+        // console.log(u1,r1);
+
     })
 }
 
